4c25132e5d8f9cc808201ccccefafd610c9fcc24
ZekTy <2553146960@qq.com>
2025-03-27 17:37:14 +0800

    修改默认的服务器和 key

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["118.178.129.54"];
+pub const RS_PUB_KEY: &str = "TyKIhc9kI0Vqo4wemcdfH+SY0zVnsrrLtGz31AiU2K8=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
